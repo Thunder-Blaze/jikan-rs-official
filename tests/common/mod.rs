a7561cf167fffc3c3b86710578ad2c201f9b1150
ThunderBlaze <darkthunderstormshiva@gmail.com>
2025-05-12 09:13:04 +0530

    Even Faster ?

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -3,5 +3,5 @@ use tokio::time::sleep;
 
 // Helper function to handle rate limiting between tests
 pub async fn wait_between_tests() {
-    sleep(Duration::from_secs(2)).await;
+    sleep(Duration::from_millis(1200)).await;
 }
